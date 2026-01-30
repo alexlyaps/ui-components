614b74023a507f5fc19fc77061ddc48ed0ad8e3d
alexlyaps <a34270622@gmail.com>
2026-01-31 00:21:59 +0100

    install tailwind

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,7 +2,11 @@ import type { ButtonProps as Props } from './button.types'
 
 export const Button = ({ children, onClick, disabled }: Props) => {
   return (
-    <button onClick={onClick} disabled={disabled}>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
+    >
       {children}
     </button>
   )
