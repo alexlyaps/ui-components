f6632000a79eb8d2f3cc31d4425158deba6043d9
alexlyaps <a34270622@gmail.com>
2026-01-30 18:14:57 +0100

    lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,16 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+// figure out why .storybook/main.js falls
 import storybook from "eslint-plugin-storybook";
 
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import tseslint, { parser } from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores([
-    "dist",
-    "node_modules",
-    "*.config.*",
-    ".storybook",
-    "vitest.*",
-  ]),
+  globalIgnores(["dist", "node_modules", ".storybook"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
@@ -33,8 +28,16 @@ export default defineConfig([
     },
   },
   {
-    files: ["**/*.test.{ts,tsx"],
-    globals: globals.node,
+    files: ["**/*.test.{ts,tsx}"],
+  },
+  {
+    files: ["vitest.shims.d.ts", "vite.config.ts", "vitest.*.ts"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.vitest },
+      parserOptions: {
+        project: "./tsconfig.node.json",
+      },
+    },
   },
   ...storybook.configs["flat/recommended"],
 ]);
